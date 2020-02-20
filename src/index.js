c9f306d5da606e82f6679201b8bbc36f9aedadf5
dima xmiel <dima.hmelios@gmail.com>
2020-02-20 10:51:12 +0100

    feat: solve typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+exports.min = function min(array) {
+    return (array && array.length && Math.min(...array)) || 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return (array && array.length && Math.max(...array)) || 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return (
+        (array &&
+            array.length &&
+            array.reduce((a, b) => a + b, 0) / array.length) ||
+        0
+    );
+};
